525c665c8ce61513ef1a46e0acaf920bdccf372b
jackiejiaqiliu <90330977+jackiejiaqiliu@users.noreply.github.com>
2022-09-22 09:55:53 -0400

    Delete numerical_input.sh

--- Lab 3/numerical_input.sh
+++
@@ -1,8 +0,0 @@
-#from https://elinux.org/RPi_Text_to_Speech_(Speech_Synthesis)
-espeak -ven+f2 -k5 -s150 --stdout "What is your zipcode?" | aplay
-
-#arecord -f cd -r 16000 -d 5 -t wav recorded.wav && sox recorded.wav recorded_mono.wav remix 1,2
-
-arecord -D hw:1,0 -f cd -c1 -r 16000 -d 5 -t wav answer.wav
-python3 test_words.py answer.wav
-
